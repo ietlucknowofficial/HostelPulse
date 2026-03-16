5ca3cbcc93258077b677bb28bcdad9776585eca4
Abhinav-1016 <abhinav9129885700@gmail.com>
2026-03-16 21:59:36 +0530

    updated student route

--- server/routes/student.routes.mjs
+++ server/routes/student.routes.mjs
@@ -3,7 +3,9 @@ import {authMiddleware } from "../middleware/auth.middleware.mjs"
 
 import  {completeProfile} from "../controllers/student.controller.mjs";
 import { roleMiddleware } from '../middleware/role.middleware.mjs';
+import { createComplaint } from '../controllers/complaint.controller.mjs';
 const router=express.Router()
 
-router.post('/complete-profile',authMiddleware,roleMiddleware("Student"),completeProfile)
+
+router.post('/create-complaint',authMiddleware,roleMiddleware("Student"),createComplaint)
 export default router
