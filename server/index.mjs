f2588363c858498b03e3ec514ba84ad9ca871f63
Abhinav-1016 <abhinav9129885700@gmail.com>
2026-04-10 02:22:14 +0530

    added port in .env

--- server/index.mjs
+++ server/index.mjs
@@ -22,9 +22,9 @@ app.use('/api/admin',adminRoutes)
 const startServer = async () => {
   try {
     await connectDB();
-    app.listen(3000, () => {
-      console.log(`Server running on port 3000`);
-    });
+   app.listen(process.env.PORT || 3000, () => {
+  console.log(`Server running on port ${process.env.PORT || 3000}`);
+});
   } catch (error) {
     console.error("Failed to start server:", error.message);
     process.exit(1);
